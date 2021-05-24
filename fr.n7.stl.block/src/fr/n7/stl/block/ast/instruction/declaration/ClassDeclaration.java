bd8166a19e71d30a0dc0fb613adbde303a3f89f1
Guillaume Claverie <guillaume.claverie@mail.com>
2021-05-24 11:07:15 +0200

    Ajout de InterfaceDeclaration & ClassDeclaration

---
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/ClassDeclaration.java
@@ -0,0 +1,48 @@
+package fr.n7.stl.block.ast.instruction.declaration;
+
+import fr.n7.stl.block.ast.instruction.Instruction;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+public class ClassDeclaration implements Instruction, Declaration {
+    //TODO
+
+    @Override
+    public boolean collect(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean resolve(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean checkType() {
+        return false;
+    }
+
+    @Override
+    public int allocateMemory(Register _register, int _offset) {
+        return 0;
+    }
+
+    @Override
+    public Fragment getCode(TAMFactory _factory) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
