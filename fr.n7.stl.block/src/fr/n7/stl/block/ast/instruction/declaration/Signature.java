207c35c5c4ee375578319dcf11058acc4a2eb2bd
Guillaume Claverie <guillaume.claverie@mail.com>
2021-05-24 16:56:49 +0200

    InterfaceElement, Signature

---
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/Signature.java
@@ -0,0 +1,78 @@
+package fr.n7.stl.block.ast.instruction.declaration;
+
+import fr.n7.stl.block.ast.Block;
+import fr.n7.stl.block.ast.instruction.Instruction;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+import java.util.List;
+
+public class Signature implements Instruction, Declaration {
+
+    /**
+     * Name of the signature
+     */
+    protected String name;
+
+    /**
+     * AST node for the returned type of the signature
+     */
+    protected Type type;
+
+    /**
+     * List of AST nodes for the formal parameters of the signature
+     */
+    protected List<ParameterDeclaration> parameters;
+
+    /**
+     * @return the parameters
+     */
+    public List<ParameterDeclaration> getParameters() {
+        return parameters;
+    }
+
+    public Signature(String _name, Type _type, List<ParameterDeclaration> _parameters) {
+        this.name = _name;
+        this.type = _type;
+        this.parameters = _parameters;
+    }
+
+    @Override
+    public boolean collect(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean resolve(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean checkType() {
+        return false;
+    }
+
+    @Override
+    public int allocateMemory(Register _register, int _offset) {
+        return 0;
+    }
+
+    @Override
+    public Fragment getCode(TAMFactory _factory) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
