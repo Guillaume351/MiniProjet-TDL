238d8033f4b10f36e26e4654300fad339fa90cad
Guillaume Claverie <guillaume.claverie@mail.com>
2021-05-24 16:56:33 +0200

    ClassElement

--- fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/ClassElement.java
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/ClassElement.java
@@ -1,10 +1,15 @@
 package fr.n7.stl.block.ast.instruction.declaration;
 
+import fr.n7.stl.block.ast.instruction.Instruction;
 import fr.n7.stl.block.ast.scope.AccessRight;
 import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
 import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
 
-public class ClassElement implements Declaration {
+public class ClassElement implements Instruction, Declaration {
 
     AccessRight accessRight;
 
@@ -24,5 +29,30 @@ public class ClassElement implements Declaration {
 
     public void setAccessRight(AccessRight accessRight) {
         this.accessRight = accessRight;
+    }
+
+    @Override
+    public boolean collect(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean resolve(HierarchicalScope<Declaration> _scope) {
+        return false;
+    }
+
+    @Override
+    public boolean checkType() {
+        return false;
+    }
+
+    @Override
+    public int allocateMemory(Register _register, int _offset) {
+        return 0;
+    }
+
+    @Override
+    public Fragment getCode(TAMFactory _factory) {
+        return null;
     }
 }
