a0681684719e23106db8b6ff29a7ad3ff80fcdbe
GauBen <benaim.gautier@gmail.com>
2021-05-29 18:06:10 +0200

    feat: collect in callable expressions

--- fr.n7.stl.block/src/fr/n7/stl/block/ast/expression/ExpressionFunctionCall.java
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/expression/ExpressionFunctionCall.java
@@ -2,13 +2,14 @@ package fr.n7.stl.block.ast.expression;
 
 import java.util.List;
 
+import fr.n7.stl.block.ast.expression.assignable.AssignableExpression;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
 import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.TAMFactory;
 
-public class ExpressionFunctionCall implements Expression {
+public class ExpressionFunctionCall implements AssignableExpression {
 
   private Expression callable;
   private List<Expression> params;
@@ -20,20 +21,17 @@ public class ExpressionFunctionCall implements Expression {
 
   @Override
   public boolean collect(HierarchicalScope<Declaration> _scope) {
-    // TODO Auto-generated method stub
-    return false;
+    return this.callable.collect(_scope) && this.params.stream().allMatch(param -> param.collect(_scope));
   }
 
   @Override
   public boolean resolve(HierarchicalScope<Declaration> _scope) {
-    // TODO Auto-generated method stub
-    return false;
+    return this.callable.resolve(_scope) && this.params.stream().allMatch(param -> param.resolve(_scope));
   }
 
   @Override
   public Type getType() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.callable.getType();
   }
 
   @Override
