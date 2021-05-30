929916dc97c53fb36f1cbcfa38e5cff500a93588
GauBen <benaim.gautier@gmail.com>
2021-05-30 11:18:53 +0200

    feat: collect this keyword

--- fr.n7.stl.block/src/fr/n7/stl/block/ast/expression/value/ThisValue.java
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/expression/value/ThisValue.java
@@ -1,8 +1,10 @@
 package fr.n7.stl.block.ast.expression.value;
 
 import fr.n7.stl.block.ast.expression.assignable.AssignableExpression;
+import fr.n7.stl.block.ast.instruction.declaration.minijava.ClassElement;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.scope.OwnedHierarchicalScope;
 import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.TAMFactory;
@@ -12,7 +14,15 @@ public class ThisValue implements Value, AssignableExpression {
 
   @Override
   public boolean collect(HierarchicalScope<Declaration> _scope) {
-    Logger.error("this");
+    if (!(_scope instanceof OwnedHierarchicalScope)) {
+      Logger.error("`this` not in a class member");
+      return false;
+    }
+    Declaration owner = ((OwnedHierarchicalScope<Declaration>) _scope).getOwner();
+    if (!(owner instanceof ClassElement)) {
+      Logger.error("`this` not in a class member");
+      return false;
+    }
     return false;
   }
 
