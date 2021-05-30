cb43f440bd975c57d5ddd2c2eea3fda7ba63a263
GauBen <benaim.gautier@gmail.com>
2021-05-30 12:19:21 +0200

    refactor: removed DREADFUL and HORRENDOUS hacks

--- fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/Return.java
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/Return.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *
  */
 package fr.n7.stl.block.ast.instruction;
 
 import fr.n7.stl.block.ast.expression.Expression;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.scope.OwnedHierarchicalScope;
 import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.Register;
@@ -14,6 +15,7 @@ import fr.n7.stl.util.Logger;
 
 /**
  * Implementation of the Abstract Syntax Tree node for a return instruction.
+ *
  * @author Marc Pantel
  *
  */
@@ -25,51 +27,55 @@ public class Return implements Instruction {
 
 	protected int parametersSize;
 
+	private Declaration owner;
+
 	public Return(Expression _value) {
 		this.value = _value;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		//return "return " + this.value + ";\n";
+		// return "return " + this.value + ";\n";
 		return "";
 	}
-	
-	/* (non-Javadoc)
-	 * @see fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope.Scope)
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope
+	 * .Scope)
 	 */
 	@Override
 	public boolean collect(HierarchicalScope<Declaration> _scope) {
-		if (_scope.knows("return")){
-			this.returnType = (_scope.get("return")).getType();
-		} else {
-			Logger.error("Return: Il y a un soucis! La fonction n'a pas renseigné son type de retour");
+		if (!(_scope instanceof OwnedHierarchicalScope)) {
+			Logger.error("Unexpected return statement.");
+			return false;
 		}
-		if(_scope.knows("$parameterslength$")){
-			// Comme $parameterslength$ ne contient pas d'entier, ça passe ! On a juste a convertir en string,
-			// puis retirer tout ce qui ne correspond pas a la valeur...
-			// Encore une fois, c'est pas tres propre, mais on a pas besoin de rajouter de getter comme ça...
-			// Et on aime pas rajouter de getter.
-			// On aurait pu aussi jouer avec l'héritage... Mais bon, tant que ça marche ...:))))
-			this.parametersSize = Integer.parseInt(_scope.get("$parameterslength$").toString().replaceAll("[^0-9]",""));;
-		}else{
-			Logger.error("Return: Il y a un soucis! Je n'ai pas acces a la longueur des parametres :(");
-		}
-		return this.value.collect(_scope);
+		owner = ((OwnedHierarchicalScope<?>) _scope).getOwner();
+		return value.collect(_scope);
 	}
-	
-	/* (non-Javadoc)
-	 * @see fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope.Scope)
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope
+	 * .Scope)
 	 */
 	@Override
 	public boolean resolve(HierarchicalScope<Declaration> _scope) {
 		return this.value.resolve(_scope);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see fr.n7.stl.block.ast.Instruction#checkType()
 	 */
 	@Override
@@ -80,15 +86,21 @@ public class Return implements Instruction {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register, int)
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register,
+	 * int)
 	 */
 	@Override
 	public int allocateMemory(Register _register, int _offset) {
 		return 0;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see fr.n7.stl.block.ast.Instruction#getCode(fr.n7.stl.tam.ast.TAMFactory)
 	 */
 	@Override
@@ -97,7 +109,7 @@ public class Return implements Instruction {
 
 		fragment.append(this.value.getCode(_factory));
 
-		//fragment.add(_factory.createLoadI(this.value.getType().length()));
+		// fragment.add(_factory.createLoadI(this.value.getType().length()));
 
 		int returnSize = this.value.getType().length();
 
